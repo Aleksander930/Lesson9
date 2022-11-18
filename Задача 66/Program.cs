388e79c028b093a67ae12fa84747a268fb664f14
Alexander Prilepin <aleksander930@list.ru>
2022-11-18 05:02:21 +0300

    Lesson 9

---
+++ Задача 66/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// (НЕ ИСПОЛЬЗОВАТЬ РЕЗУЛЬТИРУЮЩУЮ ПЕРЕМЕННУЮ)
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите число M: ");
+int numM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+if (numM > numN)
+{
+    int number = numM;
+    numM = numN;
+    numN = number;
+}
+int sumarr = ArrayNum(numM, numN);
+Console.WriteLine(sumarr);
+
+int ArrayNum(int numM, int numN)
+{
+    if (numM + 1 >= numN)
+    {
+        return numM + numN;
+    }
+    return numM + ArrayNum(numM + 1, numN);
+}
---
+++ Задача 68/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.Write("Введите число m: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int array = NumberMN(numberM, numberN);
+Console.WriteLine(array);
+int NumberMN(int numM, int numN)
+{
+    if (numM == 0) return numN + 1;
+    if (numM > 0 && numN == 0) return NumberMN(numM - 1, 1);
+    return NumberMN(numM - 1, NumberMN(numM, numN - 1));
+}
